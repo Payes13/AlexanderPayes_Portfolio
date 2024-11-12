43bb462f5456d083595d8b6d9bd1fbe991ea9e6b
Alexander Payes <alexander.payes.13@gmail.com>
2024-11-11 22:54:54 -0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-20 h-20 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a skilled Full Stack Web Developer and AWS Cloud Developer with a proven track record of designing and deploying robust, scalable solutions. My career has been built on a foundation of creating user-friendly website interfaces, dynamic web applications, and highly efficient cloud-based architectures.
+
+        With hands-on experience in React.js, Next.js, Node.js, and PostgreSQL, I develop seamless, responsive applications that deliver exceptional user experiences. My expertise extends to utilizing AWS services such as Elastic Beanstalk, S3, RDS, API Gateway, Lambda, and SES to build secure, scalable, and high-performing cloud-based systems.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,328 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  AIImageTransformation,
+  InventoryManagementSystem,
+  AIChatGemini,
+  TechnicalAssessment,
+  AICodexChat,
+  AppleWebsiteClone,
+  cdvi,
+  syntax
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "project",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "AWS Cloud Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "SAP Technical Consultant",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  }
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer and AWS Cloud Developer",
+    company_name: "CDVI Americas",
+    icon: cdvi,
+    iconBg: "#383E56",
+    date: "Jan 2020 - April 2022",
+    points: [
+      "Utilized React.js and Next.js to build dynamic, responsive user interfaces, integrated with Material UI and Tailwind CSS for a consistent design system.",
+      "Developed and optimized backend services using Node.js and pg-promise for secure and efficient interaction with PostgreSQL databases.",
+      "Deployed and managed scalable web applications using AWS Elastic Beanstalk, ensuring optimal performance and availability.",
+      "Configured and optimized Amazon S3 buckets for secure storage solutions and efficient content delivery.",
+      "More on Resume's work experience"
+    ],
+  },
+  {
+    title: "SAP Technical Consultant - Cloud Platform Integration",
+    company_name: "Syntax",
+    icon: syntax,
+    iconBg: "#E6DEDD",
+    date: "April 2022 - Present",
+    points: [
+      "Designing and developing SAP CPI Integration Solutions using adapters such as: HTTP, SFTP, OData, SOAP, Process Direct, Salesforce, etc.",
+      "Transforming input payload through mapping standard and customized functions using Groovy programming language.",
+      "I developed Groovy scripts to implement custom data transformations, ensuring seamless and efficient integration of complex datasets.",
+      "Assisting development teams in designing, developing, and testing SAP Integration interfaces.",
+      "More on Resume's work experience"
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "AI Image Transformation",
+    description:
+      "Web application that transforms images using Artificial Intelligence. Some features include remove object from an image, remove background, recolor objects, generate fill, refine image, and you have the option to purchase more credits to keep using the service.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "cloudinary AI API",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "stripe API",
+        color: "black-gradient",
+      },
+    ],
+    image: AIImageTransformation,
+    source_code_link: "https://github.com/Payes13/NXTWebsite_AI_Image_TransformationJSM",
+  },
+  {
+    name: "Inventory Management Dashboard",
+    description:
+      "Web application that provides users with comprehensive inventory management. The application enables users to access inventory data, track product sales and purchases, monitor expenses, and visualize sales summaries through interactive charts. Key features include dedicated pages for inventory management, product catalog, user administration, and customizable settings.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "AWS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "postgresql",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "black-gradient",
+      },
+    ],
+    image: InventoryManagementSystem,
+    source_code_link: "https://github.com/Payes13/NXTWebsite_InventoryER",
+  },
+  {
+    name: "AI Chat Gemini",
+    description:
+      "Interactive application that enables users to engage in dynamic conversations with AI. Users can ask any question, and the AI will provide thoughtful responses. Additionally, the application supports image input, allowing users to upload images and receive AI-driven insights and answers related to the content of the image.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "gemini AI API",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: AIChatGemini,
+    source_code_link: "https://github.com/Payes13/AIChatGoogleGemini/tree/master",
+  },
+  {
+    name: "Technical Assessment",
+    description:
+      "This application allows you to fetch users from an API and display their information in a dynamic and interactive interface. Key features include row coloring for enhanced visualization, sorting functionality by name, last name, or country, as well as the ability to filter users by country. Users can also remove individual entries, reset to the original dataset.",
+    tags: [
+      {
+        name: "typescript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "reactjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "random user API",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: TechnicalAssessment,
+    source_code_link: "https://github.com/Payes13/FEWebsite_fetchUsersMid",
+  },
+  {
+    name: "AI ChatGPT Codex",
+    description:
+      "AI-powered chat assistant designed to support users with coding-related questions by leveraging OpenAI's ChatGPT API. The assistant offers personalized guidance, answers programming queries, and helps users enhance their coding skills through engaging coding challenges.",
+    tags: [
+      {
+        name: "nodejs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "reactjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chatgpt AI API",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: AICodexChat,
+    source_code_link: "https://github.com/Payes13/FSWebsite_openAI_chat_assistantJSM",
+  },
+  {
+    name: "Apple Website Clone",
+    description:
+      "Apple website clone, a visually striking clone of the Apple website using React.js, GSAP animations, and Three.js. Leveraging these powerful libraries, I created stunning 3D models and animations seamlessly integrated into the web pages, delivering an engaging and interactive user experience.",
+    tags: [
+      {
+        name: "threejs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "reactjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "GSAP animation library",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: AppleWebsiteClone,
+    source_code_link: "https://github.com/Payes13/FEWebsite_AppleCloneJSM",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
